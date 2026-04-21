b820a2d16385e9c56f28c91c091d00c73de34660
YalukR <yalukramos@gmail.com>
2026-04-20 20:49:50 -0600

    admin rol

--- apps/roles/admin.py
+++ apps/roles/admin.py
@@ -2,14 +2,19 @@
 from django.contrib import admin
 from .models import Permiso, Rol, RolPermiso
 
-@admin.register(RolPermiso)
-class RolPermisoAdmin(admin.ModelAdmin):
-    list_display = ('rol', 'permiso')
-    list_filter = ('rol', 'permiso')
-    
-# apps/roles/admin.py
+@admin.register(Permiso)
+class PermisoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'modulo', 'descripcion')
+    list_filter = ('modulo',)
+    search_fields = ('codigo',)
+
 @admin.register(Rol)
 class RolAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'tenant', 'activo')
     list_filter = ('activo',)
     search_fields = ('nombre', 'tenant__nombre_negocio')
+    
+@admin.register(RolPermiso)
+class RolPermisoAdmin(admin.ModelAdmin):
+    list_display = ('rol', 'permiso')
+    list_filter = ('rol', 'permiso')
